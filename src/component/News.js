0610ce9ddcaecc3a02b3e2c69182bc548c670008
Krushna Chaudhari <krushnabhusawal@gmail.com>
2023-08-10 16:54:07 +0530

    Previous and Next Btn added

--- src/component/News.js
+++ src/component/News.js
@@ -1,25 +1,55 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 export default class News extends Component {
-  article = [
-
-  ];
+  article = [];
   constructor() {
     super();
     this.state = {
       article: this.article,
-     };
+      page: 1,
+      totalResults :0
+    };
   }
   //async and await is used with componentDidMount() method to fetch api results
-  async componentDidMount(){
-  
-      let url ="https://newsapi.org/v2/top-headlines?country=in&apiKey=72af4f79794b40d5937a59aa7f0dbddb";
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=72af4f79794b40d5937a59aa7f0dbddb&page=1&pagesize=21";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      article: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
+
+  handleNext = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 21)) {
+     alert("No More News are available to show.....")
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=72af4f79794b40d5937a59aa7f0dbddb&page=${
+        this.state.page + 1
+      }&pagesize=21`;
       let data = await fetch(url);
       let parsedData = await data.json();
-      this.setState({article:parsedData.articles})
-    
-   
-  }
+      this.setState({
+        article: parsedData.articles,
+        page: this.state.page + 1,
+      });
+    }
+  };
+
+  handlePrev = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=72af4f79794b40d5937a59aa7f0dbddb&page=${
+      this.state.page - 1
+    }&pagesize=21`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      article: parsedData.articles,
+      page: this.state.page - 1,
+    });
+  };
+
   render() {
     return (
       <>
@@ -27,16 +57,39 @@ export default class News extends Component {
           <h2 className="my-3 mx-2">News Update - Top Headlines</h2>
           <div className="row">
             {this.state.article.map((element) => {
-              return <div className="col-md-4" key={element.url}>
-                <NewsItem
-                //Terniry opeartors are used to handle the null data
-                  title={element.title?element.title.slice(0,45):" "}
-                  description={element.description?element.description.slice(0,88):" "}
-                  imageUrl={element.urlToImage}
-                  newsUrl ={element.url}
-                />
-              </div>;
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    //Terniry opeartors are used to handle the null data
+                    title={element.title ? element.title.slice(0, 45) : " "}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : " "
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
             })}
+          </div>
+          <div className="container d-flex justify-content-between">
+            <button
+              type="button"
+              onClick={this.handlePrev}
+              disabled={this.state.page <= 1}
+              className="btn btn-success"
+            >
+              Previous
+            </button>
+            <button
+              type="button"
+              onClick={this.handleNext}
+              className="btn btn-success"
+            >
+              Next
+            </button>
           </div>
         </div>
       </>
